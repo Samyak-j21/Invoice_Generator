0def1feea67a10deddc10905e1a4b0a988ae9dae
Samyak-j21 <samyakhain123@gmail.com>
2025-09-19 23:13:30 +0530

    new change 1

--- src/diff_components/Buttons.js
+++ src/diff_components/Buttons.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import '../diff_styles/Buttons.css';
 
-export default function Buttons({ onSave, onPrint, onDownload }) {
+export default function Buttons({ onPrint }) {
   return (
     <div className="buttons-container">
-      <button onClick={onSave} className="button save-button">
-        Save
-      </button>
       <button onClick={onPrint} className="button print-button">
         Print
-      </button>
-      <button onClick={onDownload} className="button download-button">
-        Download PDF
       </button>
     </div>
   );
